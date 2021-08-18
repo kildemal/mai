1b7f5268db8c0ca62415b9a1005563568cf4c53b
kildemal <kildemal@protonmail.com>
2021-08-18 18:44:49 +0530

    added basic code to get the bot online

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import discord
+import os
+from discord.ext import commands
 
+
+PREFIX = 'mai '
+INTENTS = discord.Intents().all()
+TOKEN = os.environ['TOKEN']
+bot = commands.Bot(command_prefix=PREFIX, intents=INTENTS)
+
+
+@bot.event
+async def on_ready:
+    print('bot online')
+
+bot.run(TOKEN)
