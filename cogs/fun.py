1e51d1a79f4c0197a5f037602020cce00e217a04
kildemal <bharathps37@gmail.com>
2021-09-11 18:39:09 +0530

    Added more functinality to randomping

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,6 +10,8 @@ def is_online(self):
         return True
 
 
+message = "Hey, what's going on. Chat looking awfully dead huh?"
+
 discord.Member.is_online = is_online
 
 
@@ -19,7 +21,7 @@ class Fun(commands.Cog):
 
     # Commands
 
-    @commands.command()
+    @commands.command()  # Displays a random anime quote
     async def aniquote(self, ctx):
         async with aiohttp.ClientSession() as session:
 
@@ -32,20 +34,20 @@ class Fun(commands.Cog):
 
         await ctx.send(quote)
 
-    @commands.command(name="is_online", aliases=["io"])
+    @commands.command(name="is_online", aliases=["io"])  # Shows if a member is online
     async def check_status(self, ctx, member: discord.Member):
         if member.is_online():
             await ctx.send('member online')
         else:
             await ctx.send('member not online')
 
-    @commands.command(name="randping", aliases=["rp"])
+    @commands.command(name="randping", aliases=["rp"])  # Pings random online members
     @commands.has_permissions(manage_guild=True)
-    async def rand_ping(self, ctx, to_ping=1):
+    async def rand_ping(self, ctx, to_ping=1, msg: str = message):
         online_members = [member.mention for member in ctx.guild.members
                           if member.is_online() if not member.bot]
         ping_list = random.sample(online_members, to_ping)
-        await ctx.send(' '.join(ping_list))
+        await ctx.send(' '.join(ping_list) + ' ' + msg)
 
 
 def setup(bot):
